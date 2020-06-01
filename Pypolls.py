01c771e4c2211722a85c4c9843d927c1c179f904
Britt Boughey <britt.boughey@lanzatech.com>
2020-05-31 23:23:59 -0700

    Adding the election audit code.

--- Pypolls.py
+++ Pypolls.py
@@ -29,9 +29,23 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#1. Initialize a total vote counter.
+total_votes = 0
+
+# Print the candidate name from each row
+candidate_options = []
+
+#Declare an empty dictionary
+candidate_votes = {}
+
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
-
+    file_reader = csv.reader(election_data)
 
 # Using the open() function with the "w" mode we will write data to the file.
 #open(file_to_save, "w")
@@ -55,8 +69,68 @@ with open(file_to_load) as election_data:
         #To Do: read and analyze the data here.
     
     #Open the election results and read the file.
-    file_reader = csv.reader(election_data)
+   
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    #Print each row in the CSV file
+    for row in file_reader:
+        #2 add to the total vote count
+        total_votes += 1
+
+        #Print the candidate name from each row.
+        candidate_name = row[2]
+
+        #If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            #Add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+            #Begin tracking the candidates vote count
+            candidate_votes[candidate_name] = 0
+
+        #Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
+
+
+#Determine the percentage of votes for each candidate by looping through the counts.
+#1. Iterate through the candidate list.
+for candidate in candidate_votes:
+    #2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate]
+    #3. Calcualte the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    #4. Print the candidate's name, vote count, and percentage of votes to the terminal
+    #print(f"{candidate}: received {vote_percentage:.1f}% of the vote.") 
+
+    # Determine winning vote count and candidate
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+    # If true then set winning_count = votes and winning_percent =
+    # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate  
+
+        # To do: print out each candidate's name, vote count, and percentage of
+        # votes to the terminal.
+    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
+# 3. Print the total votes.
+#print(total_votes)
+
+#Print the candidate list.
+#print(candidate_options)
+
+#Print the candidate vote dictionary.
+#print(candidate_votes)
---
+++ Python_practice.py
@@ -0,0 +1,35 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")    
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+
+
+for i in range(len(counties)):
+    print(counties[i])
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (
+    f"You received {candidate_votes} number of votes. "
+    f"The total number of votes in the election was {total_votes}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
+
+print(message_to_candidate)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
